194e85ac8531d5ce44833e2363d27f3220957c5b
Levi Thomason <me@levithomason.com>
2015-04-10 17:08:25 -0700

    more

--- app/app.js
+++ app/app.js
@@ -8,16 +8,8 @@ angular.module('App', [
     'firebase',
 
     // app
-    'App.view1',
+    'App.lists',
 ])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }])
-
-    .controller('Controller', function ($scope, $firebaseObject) {
-        var ref = new Firebase('https://blazing-torch-8548.firebaseio.com');
-
-        $scope.data = $firebaseObject(ref);
-
-        console.log($scope.data);
-    });
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
