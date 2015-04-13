304d5768f0bd8b6cadf70377bc90cce186550f4d
levithomason <me@levithomason.com>
2015-04-12 20:39:20 -0700

    new list form started

--- app/components/new-list/new-list-directive.js
+++ app/components/new-list/new-list-directive.js
@@ -6,12 +6,39 @@ function newList() {
         scope: {},
         templateUrl: 'app/components/new-list/new-list.html',
         link: function(scope, elem, attrs) {
+            var form = angular.element(document.getElementsByClassName('lst-new-list-form'));
+            scope.listTypes = ['Poll', 'Checklist'];
+            scope.listType = 'Poll';
 
+            scope.createList = function() {
+                
+            };
+
+            scope.setListType = function(type) {
+                scope.listType = type;
+            };
+
+            $rootScope.$on('lst-new-list-button-clicked', function() {
+                form.toggleClass('lst-show-form');
+            });
+        }
+    }
+}
+
+function newListButton() {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {},
+        templateUrl: 'app/components/new-list-button/new-list-button.html',
+        link: function(scope, elem, attrs) {
             scope.newClicked = function() {
-                elem.toggleClass('lst-show-form');
+                $rootScope.$broadcast('lst-new-list-button-clicked');
             };
         }
     }
 }
+
 angular.module('App.newList')
-    .directive('newList', newList);
+    .directive('newList', newList)
+    .directive('newListButton', newListButton);
