4d4ce5a788af4bba19a60e0bda11243d9606a98c
levithomason <me@levithomason.com>
2015-06-18 19:54:35 -0700

    reorganize code, add public lists and make button

--- app/app.js
+++ app/app.js
@@ -1,33 +1,21 @@
 'use strict';
 
 angular.module('App', [
-    // ng deps
-    'ngRoute',
+  // ng deps
+  'ngRoute',
 
-    // vendor deps
-    'firebase',
-    'ngClipboard'
+  // vendor deps
+  'firebase',
+  'ngClipboard'
 ])
 
-    .constant('FIREBASE_ROOT', 'https://ridiculist.firebaseio.com/')
+  .constant('FIREBASE_ROOT', 'https://ridiculist.firebaseio.com/')
 
-    .config(['ngClipProvider', function(ngClipProvider) {
-        ngClipProvider.setPath('app/bower/zeroclipboard/dist/ZeroClipboard.swf');
-    }])
+  .config(['ngClipProvider', function (ngClipProvider) {
+    ngClipProvider.setPath('app/bower/zeroclipboard/dist/ZeroClipboard.swf');
+  }])
 
-    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-        $routeProvider.otherwise({redirectTo: '/'});
-        $locationProvider.html5Mode(true).hashPrefix('!');
-    }])
-    .controller('AppController', ['$scope', 'FIREBASE_ROOT', '$firebaseObject',
-        function($scope, FIREBASE_ROOT, $firebaseObject) {
-            var publicListsRef = new Firebase(FIREBASE_ROOT).child('lists');
-
-            $scope.lists = $firebaseObject(publicListsRef);
-
-            $scope.loadList = function(id) {
-                $location.path(id);
-            };
-
-        }])
-;
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true).hashPrefix('!');
+  }]);
--- app/views/home/home.js
+++ app/views/home/home.js
@@ -2,19 +2,24 @@
 
 angular.module('App')
 
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/:id', {
-                templateUrl: '/app/views/home/home.html',
-                controller: 'Controller'
-            })
-            .when('/', {
-                templateUrl: '/app/views/home/home.html',
-                controller: 'Controller'
-            });
-    }])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/:id', {
+        templateUrl: '/app/views/home/home.html',
+        controller: 'Controller'
+      })
+      .when('/', {
+        templateUrl: '/app/views/home/home.html',
+        controller: 'Controller'
+      });
+  }])
 
-    .controller('Controller', ['$scope', '$firebaseObject', '$routeParams', '$location', '$rootScope', 'FIREBASE_ROOT',
-        function($scope, $firebaseObject, $routeParams, $location, $rootScope, FIREBASE_ROOT) {
-        }]);
+  .controller('Controller', ['$scope', 'FIREBASE_ROOT', '$firebaseObject', '$routeParams',
+    function ($scope, FIREBASE_ROOT, $firebaseObject, $routeParams) {
+      var publicListsRef = new Firebase(FIREBASE_ROOT).child('lists');
+      $scope.lists = $firebaseObject(publicListsRef);
+
+      $scope.idUrlParam = $routeParams.id;
+
+    }]);
 
