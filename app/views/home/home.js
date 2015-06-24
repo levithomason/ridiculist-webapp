6c37c2004ecc483f866b81c7a0115b14ce4a3d8f
levithomason <me@levithomason.com>
2015-06-24 01:05:21 -0700

    drop search

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -2,7 +2,7 @@
 
 angular.module('App')
 
-  .config(['$routeProvider', function ($routeProvider) {
+  .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/:id', {
         templateUrl: '/app/views/home/home.html',
@@ -14,10 +14,10 @@ angular.module('App')
       });
   }])
 
-.controller('Controller', ['$scope', 'FIREBASE', '$firebaseObject', '$routeParams', 'LIST_ICONS',
-    function ($scope, FIREBASE, $firebaseObject, $routeParams, LIST_ICONS) {
+  .controller('Controller', ['$scope', 'FIREBASE', '$firebaseObject', '$routeParams', 'LIST_ICONS',
+    function($scope, FIREBASE, $firebaseArray, $routeParams, LIST_ICONS) {
       var publicListsRef = new Firebase(FIREBASE.lists);
-      $scope.lists = $firebaseObject(publicListsRef);
+      $scope.lists = $firebaseArray(publicListsRef);
       $scope.LIST_ICONS = LIST_ICONS;
 
       $scope.idUrlParam = $routeParams.id;
